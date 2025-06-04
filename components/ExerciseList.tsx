60c13eb4c0f06db88cbee6f598975ba658938233
anilyavas <anil.yavas99@gmail.com>
2025-06-05 00:28:53 +0300

    Added search functionality to ExerciseList component

--- components/ExerciseList.tsx
+++ components/ExerciseList.tsx
@@ -1,11 +1,25 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, TextInput, Pressable, Text } from 'react-native';
 import { LegendList } from '@legendapp/list';
 import exercises from '~/assets/data/exercises.json';
+import { useState } from 'react';
+import { Exercise } from '~/types/Exercise';
 
 export default function ExerciseList() {
+  const [searchInput, setSearchInput] = useState('');
+  const [filtered, setFiltered] = useState<Exercise[] | any>(exercises);
+
+  const handleSearch = (input: string) => {
+    setSearchInput(input);
+    const filteredData = exercises.filter((exercise: Exercise) =>
+      exercise.name.toLowerCase().includes(input.toLowerCase())
+    );
+    setFiltered(filteredData);
+  };
+
   return (
     <View style={styles.container}>
       <LegendList
+        style={{ flex: 1 }}
         data={exercises}
         renderItem={({ item }) => <View style={styles.exerciseContainer}></View>}
         keyExtractor={(item) => item.id}
